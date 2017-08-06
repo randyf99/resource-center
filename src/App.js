9413b770da474489643654476d48d14ef0b676db
Randy Ferrer <randfer@gmail.com>
2017-08-06 14:29:13 -0400

    Updated material ui, added some styling via theme

--- src/App.js
+++ src/App.js
@@ -1,30 +1,74 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
 import MenuItem from 'material-ui/MenuItem';
 import Drawer from 'material-ui/Drawer';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { MuiThemeProvider } from 'material-ui/styles';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import './App.css';
+import fusTheme from './fusTheme';
+
+injectTapEventPlugin();
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false,
-      selectedIndex: 0
+      open: false
     };
   }
+
+  handleClose = () => this.setState({ open: false });
+
   render() {
     return (
-      <div className="App">
-        <AppBar
-          title="Title"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-          onLeftIconButtonTouchTap={() => this.setState({ open: !this.state.open })}
-        />
-        <Drawer open={this.state.open} docked={false} onRequestChange={open => this.setState({ open })}>
-          <MenuItem>Menu Item</MenuItem>
-          <MenuItem>Menu Item 2</MenuItem>
-        </Drawer>
-      </div>
+      <BrowserRouter>
+        <MuiThemeProvider muiTheme={getMuiTheme(fusTheme)}>
+          <div>
+            <AppBar
+              title="Title"
+              iconClassNameRight="muidocs-icon-navigation-expand-more"
+              onLeftIconButtonTouchTap={() => this.setState({ open: !this.state.open })}
+            />
+            <Drawer open={this.state.open} docked={false} onRequestChange={open => this.setState({ open })}>
+              <NavLink to="/" activeClassName="selected">
+                <MenuItem onTouchTap={this.handleClose} primaryText={'Home'} />
+              </NavLink>
+              <NavLink to="/logos" activeClassName="selected">
+                <MenuItem onTouchTap={this.handleClose} primaryText={'Logos'} />
+              </NavLink>
+              <NavLink to="/posters" activeClassName="selected">
+                <MenuItem onTouchTap={this.handleClose}>Posters</MenuItem>
+              </NavLink>
+              <NavLink to="/letterhead" activeClassName="selected">
+                <MenuItem onTouchTap={this.handleClose}>Letterhead</MenuItem>
+              </NavLink>
+              <NavLink to="/share-a-story" activeClassName="selected">
+                <MenuItem onTouchTap={this.handleClose}>Share a Story</MenuItem>
+              </NavLink>
+              <NavLink to="/service-request-form" activeClassName="selected">
+                <MenuItem onTouchTap={this.handleClose}>Service Request Form</MenuItem>
+              </NavLink>
+              <NavLink to="/tutorial" activeClassName="selected">
+                <MenuItem onTouchTap={this.handleClose}>Tutorial</MenuItem>
+              </NavLink>
+            </Drawer>
+
+            <Route exact path="/" render={() => <h1>Home View</h1>} />
+            <Route exact path="/logos-posters" render={() => <h1>Logos and Posters View</h1>} />
+            <Route exact path="/logos" render={() => <h1>Logos View</h1>} />
+            <Route exact path="/posters" render={() => <h1>Posters View</h1>} />
+            <Route exact path="/letterhead" render={() => <h1>Letterhead View</h1>} />
+            <Route exact path="/share-a-story" render={() => <h1>Share a Story View</h1>} />
+            <Route exact path="/planning-guide" render={() => <h1>Planning Guide View</h1>} />
+            <Route exact path="/services" render={() => <h1>Services View</h1>} />
+            <Route exact path="/glossary" render={() => <h1>Glossary View</h1>} />
+            <Route exact path="/service-request-form" render={() => <h1>Service Request Form View</h1>} />
+            <Route exact path="/tutorial" render={() => <h1>Tutorial View</h1>} />
+          </div>
+        </MuiThemeProvider>
+      </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MuiThemeProvider } from 'material-ui/styles';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-injectTapEventPlugin();
-
-ReactDOM.render(
-  <MuiThemeProvider>
-    <App />
-  </MuiThemeProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
